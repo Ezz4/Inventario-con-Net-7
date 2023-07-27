0aca42f7fe0bd78380c53b9ee0e4821fa230c1cb
Ezz4 <zuryta92@gmail.com>
2023-07-26 20:04:45 -0600

    Interfaces implementations

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre debe de ser requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre debe de ser maximo 60 caracteres")]
+        public  string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion debe de ser requerido")]
+        [MaxLength(100, ErrorMessage = "Descripcion debe de ser maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        public bool Estado { get; set; }
+    }
+}
